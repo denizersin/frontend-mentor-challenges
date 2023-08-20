40688ecd1792f2c4134ea66059eef30d88c14021
BRAT <brat@BRAT-Air.home>
2023-08-20 12:47:39 +0300

    new

---
+++ interactive-comments-section-main/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-purple": "rgb(83,87,182)",
+        "primary-danger":"rgb(237,99,104)"
+      }
+    },
+
+  },
+  plugins: [],
+}
+
